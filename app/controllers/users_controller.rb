638d57646461cc190da23166abb683c952a8c4d4
mathushitamaho <132318410+matsushitamaho@users.noreply.github.com>
2023-05-21 07:58:32 +0000

    Userinfo,Newbook,bookの表示完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  
+
   before_action :ensure_current_user,{only:[:edit,:update]}
-  
+
   def ensure_current_user
     @user = User.find(params[:id])
     if @user.id != current_user.id
       redirect_to user_path(current_user.id)
     end
   end
-  
+
   def new
     @book=Book.new
   end
@@ -24,11 +24,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user=User.find(params[:id])
-    @books=@user.books
-    @book=Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
-  
+
   def create
     @book=Book.new(book_params)
     @book.user_id=current_user.id
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     redirect_to book_path(@book.id)
     flash[:success]="You have created book successfully."
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -46,13 +46,13 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-    
+
     private
-    
+
     def user_params
       params.require(:user).permit(:name, :profile_image, :introduction)
     end
-    
+
     def book_params
       params.require(:book).permit(:title, :body)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,29 @@
 class User < ApplicationRecord
+  mount_uploader :profile_image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates:name,
     presence:true,
     length:{minimum:2,maximum:20},
     uniqueness:true
-    
+
   validates:introduction,
     length:{maximum:50}
-    
+
   has_many :books,dependent: :destroy
-  
+  mount_uploader :image, ImageUploader
+
+  def introduction
+    self[:introduction]
+  end
+
+  def get_profile_image(width, height)
+    profile_image.url(:thumb)
+  end
+
+  attribute :introduction, :text
+  mount_uploader :profile_image,ImageUploader
 end
